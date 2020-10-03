9dc3d93c64f753b46d8b2952613d237c50adc0ed
Krzysiek <krzykarc@gmail.com>
2020-10-03 16:21:57 +0200

    add boxes

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,40 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        adusiowe-foremki
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div>
+    <div class="forms_inputs">
+      <input placeholder="Średnica foremki" />
+      <input placeholder="Średnica docelowa" />
+    </div>
+    <div class="forms__box">
+      <textarea class="forms__text" v-model="recipe"></textarea>
+      <textarea class="forms__text" :value="newRecipe"></textarea>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    newRecipe() {
+      return this.recipe;
+    }
+  },
+  data() {
+    return {
+      recipe: ""
+    };
+  }
+};
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+.forms {
   display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
 }
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.forms__box {
+  display: flex;
 }
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.forms__text {
+  resize: none;
+  width: 50%;
 }
 </style>
