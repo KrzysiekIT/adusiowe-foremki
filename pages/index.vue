125d0d988ca284916a72ddb21660aa5bbe0d63c7
Krzysiek <krzykarc@gmail.com>
2020-10-03 17:14:43 +0200

    added dist

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
     <div class="forms_inputs">
-      <input placeholder="Średnica foremki" />
-      <input placeholder="Średnica docelowa" />
+      Średnica podstawowa:&nbsp;<input v-model="diameter.source" /><br>
+      Średnica docelowa:&nbsp;<input placeholder="Średnica docelowa" v-model="diameter.target" />
     </div>
     <div class="forms__box">
-      <textarea class="forms__text" v-model="recipe"></textarea>
+      <textarea
+        placeholder="Przepis"
+        class="forms__text"
+        v-model="recipe"
+      ></textarea>
       <textarea class="forms__text" :value="newRecipe"></textarea>
     </div>
+    <button @click="recipe = example">Przykładowy przepis</button>
   </div>
 </template>
 
@@ -15,12 +20,40 @@
 export default {
   computed: {
     newRecipe() {
-      return this.recipe;
+      let newRecipe = JSON.parse(JSON.stringify(this.recipe));
+      const r = /\d+/g;
+      let m;
+      const numbersArray = [];
+      while ((m = r.exec(this.recipe)) != null) {
+        numbersArray.unshift(m);
+      }
+      for (let i = 0; i < numbersArray.length; i++) {
+        console.log(numbersArray[i][0].length, numbersArray[i]["index"]);
+        newRecipe =
+          newRecipe.slice(0, numbersArray[i]["index"]) +
+          newRecipe.slice(
+            numbersArray[i]["index"] + numbersArray[i][0].length,
+            newRecipe.length
+          );
+        newRecipe =
+          newRecipe.slice(0, numbersArray[i]["index"]) +
+          parseInt(numbersArray[i][0]) *
+            Math.pow(this.diameter.target / this.diameter.source, 2) +
+          newRecipe.slice(numbersArray[i]["index"]);
+      }
+      console.log(numbersArray);
+      return newRecipe;
     }
   },
   data() {
     return {
-      recipe: ""
+      recipe: "",
+      diameter: {
+        source: 1,
+        target: 1
+      },
+      example:
+        "makaron tagliatelle 400 gramów\nNaturalnie smaczne Spaghetti Bolognese Knorr 1 opakowanie\nmielona wołowina 400 gramów\nkrojone pomidory z puszki 1 opakowanie\nmozzarella 1 sztuka\nząbek czosnku 2 sztuki\nświeża bazylia 2 łyżki\nolej rzepakowy 1 łyżka"
     };
   }
 };
@@ -36,5 +69,6 @@ export default {
 .forms__text {
   resize: none;
   width: 50%;
+  height: 80vh;
 }
 </style>
